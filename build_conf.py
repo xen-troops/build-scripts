851e5c63bc950b543a326654a17fdd94d7edc2b3
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2017-05-29 11:32:34 +0300

    Initial version of the build scripts
    Only daily builds are supported by now.
    
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
    Reviewed-by Andrii Anisov <Andrii_Anisov@epam.com>
    Reviewed-by Volodymyr Babchuk <Volodymyr_Babchuk@epam.com>

---
+++ build_conf.py
@@ -0,0 +1,219 @@
+import argparse
+import datetime
+import os
+import shutil
+
+import ConfigParser
+
+# define build script version file name: this is created
+# in the deploy dir after successfull build
+VERSION_FNAME = 'build-system-version_1.0'
+
+'''
+The directories used during the build:
+1. Build dir - volatile storage where the build happens,
+must be the fastest storge possible, e.g. SSD drive
+2. Storage dir - non-volatile directory, used by the builds
+and remains alive through the builds, usually HDD. Used for:
+2.1. Downloads - stores downloads made by Yocto
+2.2. Build artifacts
+2.3. Xen-troops repositories used for builds
+2.4. Holds sstate-cache
+'''
+
+TYPE_DAILY	= "dailybuild"
+TYPE_PUSH	= "on_push"
+TYPE_REQ	= "on_request"
+TYPE_RECONSTR	= "reconstruct"
+
+TYPE = [
+	TYPE_DAILY,
+	TYPE_PUSH,
+	TYPE_REQ,
+	TYPE_RECONSTR
+]
+
+BUILD_VERSIONS_FNAME = "build-versions.inc"
+BUILD_METADATA_REFS_FNAME = "metadata-revs"
+
+YOCTO_DEFAULT_TARGET = "xt-image"
+
+CFG_SECTION_GIT = "git"
+CFG_OPTION_XT_HISTORY = "xt_history_uri"
+CFG_OPTION_XT_MANIFEST = "xt_manifest_uri"
+
+CFG_SECTION_PATH = "path"
+CFG_OPTION_WORKSPACE_DIR = "workspace_base_dir"
+CFG_OPTION_STORAGE_DIR = "workspace_storage_base_dir"
+
+class BuildConf(object):
+	def get_dir_build(self):
+		return self.__workspace_base_dir
+
+	def get_dir_storage(self):
+		return self.__workspace_storage_base_dir
+
+	# URI of the git repo with build history
+	def get_uri_xt_history(self):
+		return self.__xt_history_uri
+
+	def get_dir_xt_history(self):
+		return os.path.join(self.get_dir_storage(), 'build-history')
+
+	# URI of the git repo with build manifests
+	def get_uri_xt_manifest(self):
+		return self.__xt_manifest_uri
+
+	def get_dir_xt_manifest(self):
+		return os.path.join(self.get_dir_storage(), 'build-manifest')
+
+	def get_dir_buildhistory_rel(self):
+		return self.__buildhistory_rel_dir
+
+	def get_dir_history_artifacts(self):
+		return os.path.join(self.get_dir_xt_history(),
+			self.get_dir_buildhistory_rel())
+
+	# this is the directory we deliver artifacts to
+	def get_dir_build_artifacts(self):
+		return os.path.join(self.get_dir_storage(), 'build-artifacts')
+
+	def get_dir_yocto_downloads(self):
+		return os.path.join(self.get_dir_storage(), 'downloads')
+
+	# this is where we populate sstate-cache after the build,
+	# so it can be used as SSTATE_MIRROR by others
+	def get_dir_yocto_sstate_mirror(self):
+		return os.path.join(self.get_dir_storage(), 'sstate-cache')
+
+	def get_dir_yocto_build(self):
+		return os.path.join(self.get_dir_build(), 'build')
+
+	def get_dir_yocto_rootfs(self):
+		return os.path.join(self.get_dir_yocto_build(), 'rootfs')
+
+	def get_dir_yocto_deploy(self):
+		return os.path.join(self.get_dir_yocto_build(), 'deploy')
+
+	def get_dir_yocto_log(self):
+		return os.path.join(self.get_dir_yocto_build(), 'log')
+
+	def get_dir_yocto_buildhistory(self):
+		return os.path.join(self.get_dir_yocto_build(), 'buildhistory')
+
+	def get_dir_yocto_shared_rootfs(self):
+		return os.path.join(self.get_dir_yocto_rootfs(), 'shared_rootfs')
+
+	# build options
+	def get_opt_generate_local_conf(self):
+		return self.__args.local_conf
+
+	def get_opt_do_build(self):
+		return self.__args.build_run
+
+	def get_opt_buildhistory(self):
+		return self.__args.buildhistory
+
+	def get_opt_continue_build(self):
+		return self.__args.continue_build
+
+	def get_opt_build_type(self):
+		return self.__args.build_type
+
+	def get_opt_product_type(self):
+		return 'prod-' + self.__args.product_type
+
+	def get_opt_machine_type(self):
+		return self.__args.machine_type
+
+	@staticmethod
+	def setup_dir(path, remove = False, silent=False):
+		# remove the existing one if any
+		if remove:
+			if not silent:
+				print('Removing ' + path)
+			try:
+				shutil.rmtree(path)
+			except OSError, err:
+				if err.errno != os.errno.ENOENT:
+					raise
+		try:
+			os.makedirs(path)
+		except OSError, err:
+			if err.errno != os.errno.EEXIST:
+				raise
+
+	def __parse_args(self):
+		parser = argparse.ArgumentParser()
+		required = parser.add_argument_group('required arguments')
+		required.add_argument('--build-type', choices=TYPE,
+			dest='build_type', required=True, help='Build type')
+		required.add_argument('--machine',
+			dest='machine_type', required=True, help='Machine type')
+		required.add_argument('--product',
+			dest='product_type', required=True, help='Product type')
+		parser.add_argument('--with-local-conf', action='store_true',
+			dest='local_conf', required=False, default=False,
+			help='Generate local.conf for the build')
+		parser.add_argument('--with-do-build', action='store_true',
+			dest='build_run', required=False, default=False,
+			help='Also build after initialization')
+		parser.add_argument('--with-build-history', action='store_true',
+			dest='buildhistory', required=False, default=False,
+			help='Save build history in the git repository')
+		parser.add_argument('--continue-build', action='store_true',
+			dest='continue_build', required=False, default=False,
+			help='Continue existing build if any, do not clean up')
+		parser.add_argument('--config',
+			dest='config_file', required=False,
+			help = "Use configuration file for tuning")
+		known_args, other_args = parser.parse_known_args()
+		# now that we know which build it is we can add appropriate options
+		if known_args.build_type == TYPE_RECONSTR:
+			parser.add_argument('--date',
+				dest='reconstr_date', required=True,
+				help='Date of the build to reconstruct',
+				type=lambda d: datetime.strptime(d, '%Y-%m-%d'))
+			parser.add_argument('--time',
+				dest='reconstr_time', required=True,
+				help='Time of the build to reconstruct',
+				type=lambda d: datetime.strptime(d, '%H-%M-%S'))
+		self.__args = parser.parse_args()
+
+	def set_work_config(self):
+		# place where build happens, SSD
+		self.__workspace_base_dir = os.path.join(os.sep, 'tmp', 'build-ssd')
+		# place where we store files which can be re-used
+		self.__workspace_storage_base_dir = os.path.join(os.sep, 'tmp', 'build-hdd')
+
+		# URI of the git repo with build history
+		self.__xt_history_uri = 'ssh://git@git.epam.com/epmd-aepr/build-history.git'
+		# URI of the git repo with build manifests
+		self.__xt_manifest_uri = 'https://github.com/xen-troops/meta-xt-products.git'
+
+		if self.__args.config_file:
+			config = ConfigParser.ConfigParser()
+			config.read(self.__args.config_file)
+			uri = config.get(CFG_SECTION_PATH, CFG_OPTION_WORKSPACE_DIR, 1)
+			if uri:
+				self.__workspace_base_dir = uri
+			uri = config.get(CFG_SECTION_PATH, CFG_OPTION_STORAGE_DIR, 1)
+			if uri:
+				self.__workspace_storage_base_dir = uri
+			uri = config.get(CFG_SECTION_GIT, CFG_OPTION_XT_HISTORY, 1)
+			if uri:
+				self.__xt_history_uri = uri
+			uri = config.get(CFG_SECTION_GIT, CFG_OPTION_XT_MANIFEST, 1)
+			if uri:
+				self.__xt_manifest_uri = uri
+
+	def __init__(self):
+		# get build arguments
+		self.__parse_args()
+		self.set_work_config()
+		self.__buildhistory_rel_dir = os.path.join(self.get_opt_build_type(),
+			datetime.date.today().strftime('%Y-%m-%d'),
+			self.get_opt_product_type(), self.get_opt_machine_type(),
+			datetime.datetime.now().strftime('%H-%M-%S'))
+		BuildConf.setup_dir(self.get_dir_build(), not self.__args.continue_build)
+		BuildConf.setup_dir(self.get_dir_storage())
---
+++ build_prod.py
@@ -0,0 +1,226 @@
+import datetime
+import git
+import os
+import shutil
+import subprocess
+import sys
+
+import build_conf
+
+def list_directories(path):
+	dirnames = [files for files in os.listdir(path) if os.path.isdir(os.path.join(path, files))]
+	if '.git' in dirnames:
+		dirnames.remove('.git')
+	return dirnames
+
+def copy_file(src, dst, fname):
+	src = os.path.join(src, fname)
+	dst = os.path.join(dst, fname)
+	try:
+		os.remove(dst)
+	except OSError, err:
+		if err.errno != os.errno.ENOENT:
+			raise
+	try:
+		# copy with file stats
+		shutil.copy2(src, dst)
+	except IOError, err:
+		if err.errno != os.errno.ENOENT:
+			raise
+
+def copy_dir(src, dst):
+	try:
+		shutil.rmtree(dst)
+	except OSError, err:
+		if err.errno != os.errno.ENOENT:
+			raise
+	try:
+		shutil.copytree(src, dst, True)
+	except OSError, err:
+		if err.errno != os.errno.ENOENT:
+			raise
+
+def bash_run_command(cmd):
+	ret = subprocess.call("bash -c '%s'" % cmd, shell=True)
+	if ret != 0:
+		raise Exception('Failed to run ' + cmd + ': ' + str(ret))
+
+def git_init(dir, uri):
+	print('Cloning from ' + uri)
+	build_conf.BuildConf.setup_dir(dir, remove=True, silent=True)
+	git.Repo.clone_from(uri, dir, branch='master')
+
+def git_commit(cfg, dir):
+	repo = git.Repo(dir)
+	repo.heads.master.checkout()
+	repo.git.add('.')
+	repo.git.commit(m=cfg.get_dir_buildhistory_rel())
+	print('---------------------- NOT PUSHING TO BUILD HISTORY REPO -------------------')
+	if False:
+		repo.remotes.origin.push()
+
+def buildhistory_init(cfg):
+	if not cfg.get_opt_buildhistory():
+		print('Not using build history')
+		return
+	print('Initializing build history git repository')
+	git_init(cfg.get_dir_xt_history(), cfg.get_uri_xt_history())
+
+def buildhistory_commit(cfg):
+	if not cfg.get_opt_buildhistory():
+		return
+	print('Commititng build history')
+	git_commit(cfg, cfg.get_dir_xt_history())
+
+def repo_init(cfg):
+	bash_run_command('repo init -u %s -b %s -m %s.xml' %
+		(cfg.get_uri_xt_manifest(), 'master',
+		 cfg.get_opt_product_type()))
+
+def repo_sync():
+	bash_run_command('repo sync -j8')
+
+def repo_populate_manifest_get_fname(cfg):
+	return cfg.get_opt_product_type() + '.xml'
+
+def repo_populate_manifest(cfg):
+	if not cfg.get_opt_buildhistory():
+		return
+		# setup build path variables
+	history_artifacts_abs_dir = cfg.get_dir_history_artifacts()
+	print('Saving current manifest to ' + history_artifacts_abs_dir)
+	if not os.path.exists(history_artifacts_abs_dir):
+		os.makedirs(history_artifacts_abs_dir)
+	bash_run_command('repo manifest -r -o ' +
+		os.path.join(history_artifacts_abs_dir,
+		repo_populate_manifest_get_fname(cfg)))
+
+def yocto_run_command(cmd):
+	src = os.path.join('xt-distro', 'oe-init-build-env')
+	if cmd == '':
+		bash_run_command('source ' + src)
+	else:
+		bash_run_command('source ' + src + ' && ' + cmd)
+
+def yocto_add_bblayer(cfg, layer):
+	yocto_run_command('bitbake-layers add-layer ' + os.path.join('..', layer))
+
+def build_populate_artifacts(cfg):
+	dest = os.path.join(cfg.get_dir_build_artifacts(),
+		cfg.get_dir_buildhistory_rel())
+	print('Populating build artifacts to ' + dest)
+	cfg.setup_dir(dest, remove=True, silent=True)
+	# touch version file
+	os.close(os.open(os.path.join(dest, build_conf.VERSION_FNAME),
+		os.O_CREAT | os.O_TRUNC))
+	# images and sdk
+	base_dir = cfg.get_dir_yocto_deploy()
+	print("Populating images and SDK's")
+	images_list = list_directories(base_dir)
+	for image in images_list:
+		print('\tFound ' + image)
+		artifact_list = list_directories(os.path.join(base_dir, image))
+		for artifact in artifact_list:
+			if artifact in ['images', 'sdk']:
+				src = os.path.join(base_dir, image, artifact)
+				dst = os.path.join(dest, image, artifact)
+				print('\t\tPopulating ' + artifact)
+				copy_dir(src, dst)
+	# buildhistory
+	base_dir = cfg.get_dir_yocto_buildhistory()
+	print('Populating build history')
+	# touch version file
+	os.close(os.open(os.path.join(base_dir, build_conf.VERSION_FNAME),
+		os.O_CREAT | os.O_TRUNC))
+	images_list = list_directories(base_dir)
+	for image in images_list:
+		print('\tFound ' + image)
+		# copy to build artifacts
+		src = os.path.join(base_dir, image)
+		dst = os.path.join(dest, image)
+		copy_file(src, dst, build_conf.BUILD_VERSIONS_FNAME)
+		copy_file(src, dst, build_conf.BUILD_METADATA_REFS_FNAME)
+		# copy to buildhistory git repo
+		dst = os.path.join(cfg.get_dir_history_artifacts(), image)
+		cfg.setup_dir(dst, remove=True, silent=True)
+		copy_file(src, dst, build_conf.BUILD_VERSIONS_FNAME)
+		copy_file(src, dst, build_conf.BUILD_METADATA_REFS_FNAME)
+	# logs
+	print('Populating logs')
+	copy_dir(cfg.get_dir_yocto_log(), os.path.join(dest, 'logs'))
+	# manifest
+	print('Populating ' + repo_populate_manifest_get_fname(cfg))
+	copy_file(cfg.get_dir_history_artifacts(), dest,
+		repo_populate_manifest_get_fname(cfg))
+
+def build_init(cfg):
+	repo_init(cfg)
+	repo_sync()
+	# create build dir and make initial setup
+	yocto_run_command('')
+	if cfg.get_opt_generate_local_conf():
+		print('Generating local.conf')
+		f = open(os.path.join('build', 'conf', 'local.conf'), "w+t")
+		f.write('MACHINE = "' + cfg.get_opt_machine_type() + '"\n')
+		f.write('DL_DIR = "' + cfg.get_dir_yocto_downloads() + '"\n')
+		f.write('IMAGE_ROOTFS = "' + cfg.get_dir_yocto_rootfs() + '"\n')
+		f.write('DEPLOY_DIR = "' + cfg.get_dir_yocto_deploy() + '"\n')
+		f.write('BUILDHISTORY_DIR = "' + cfg.get_dir_yocto_buildhistory() + '"\n')
+		f.write('XT_SSTATE_CACHE_MIRROR_DIR = "' + cfg.get_dir_yocto_sstate_mirror() + '"\n')
+		f.write('XT_SHARED_ROOTFS_DIR = "' + cfg.get_dir_yocto_shared_rootfs() + '"\n')
+		f.write('LOG_DIR = "' + cfg.get_dir_yocto_log() + '"\n')
+		f.close()
+	# add meta layers
+	bblayers_list = list_directories(cfg.get_dir_build())
+	for bblayer in bblayers_list:
+		if bblayer.startswith('meta-'):
+			yocto_add_bblayer(cfg, bblayer)
+
+def build_run(cfg):
+	buildhistory_init(cfg)
+	bb_target = build_conf.YOCTO_DEFAULT_TARGET
+	print('Building bitbake target: ' + bb_target)
+	# repo init + sync
+	os.chdir(cfg.get_dir_build())
+	if not cfg.get_opt_continue_build():
+		build_init(cfg)
+	if not (cfg.get_opt_do_build() or cfg.get_opt_continue_build()):
+		return
+	# ready for the build
+	yocto_run_command('bitbake ' + bb_target)
+	repo_populate_manifest(cfg)
+	build_populate_artifacts(cfg)
+	buildhistory_commit(cfg)
+
+def build_print_target(build_type, cfg):
+	print('Type: %s product: %s machine: %s' % (build_type,
+		cfg.get_opt_product_type(), cfg.get_opt_machine_type()))
+
+def build_daily(cfg):
+	build_print_target(build_conf.TYPE_DAILY, cfg)
+	build_run(cfg)
+
+def build_push(cfg):
+	build_print_target(build_conf.TYPE_PUSH, cfg)
+
+def build_req(cfg):
+	build_print_target(build_conf.TYPE_REQ, cfg)
+
+def build_reconstr(cfg):
+	build_print_target(build_conf.TYPE_RECONSTR, cfg)
+
+def main():
+	try:
+		cfg = build_conf.BuildConf()
+		action = {
+			build_conf.TYPE_DAILY	: build_daily,
+			build_conf.TYPE_PUSH	: build_push,
+			build_conf.TYPE_REQ	: build_req,
+			build_conf.TYPE_RECONSTR: build_reconstr,
+		}
+		action[cfg.get_opt_build_type()](cfg)
+	finally:
+		print("Done")
+
+if __name__ == '__main__':
+	main()
